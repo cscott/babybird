d1decf70a652596b24ad3853827fba8f0d323fd5
C. Scott Ananian <cscott@cscott.net>
2015-12-03 17:06:59 -0500

    Fixes for node 0.10.

--- test/test262.js
+++ test/test262.js
@@ -32,8 +32,40 @@ var BLACKLIST = [
   // breaking compatibility with pre-ES5 code.
   'built-ins/Promise/prototype/catch/name.js',
 ];
+// Node 0.10 doesn't set the descriptor correctly for 'name' and 'length'
+// properties of functions.  There's not much we can do about it, other
+// than skip those tests.
+if (!Object.getOwnPropertyDescriptor(function f() {}, 'length').configurable) {
+  BLACKLIST.push(
+    'built-ins/Promise/all/length.js',
+    'built-ins/Promise/all/name.js',
+    'built-ins/Promise/all/resolve-element-function-length.js',
+    'built-ins/Promise/executor-function-length.js',
+    'built-ins/Promise/length.js',
+    'built-ins/Promise/name.js',
+    'built-ins/Promise/prototype/catch/length.js',
+    'built-ins/Promise/prototype/catch/name.js',
+    'built-ins/Promise/prototype/then/length.js',
+    'built-ins/Promise/prototype/then/name.js',
+    'built-ins/Promise/race/length.js',
+    'built-ins/Promise/race/name.js',
+    'built-ins/Promise/reject-function-length.js',
+    'built-ins/Promise/reject/length.js',
+    'built-ins/Promise/reject/name.js',
+    'built-ins/Promise/resolve-function-length.js',
+    'built-ins/Promise/resolve/length.js',
+    'built-ins/Promise/resolve/name.js'
+  );
+  // There are similar bugs with Object.isExtensible
+  BLACKLIST.push(
+    'built-ins/Promise/all/resolve-element-function-extensible.js',
+    'built-ins/Promise/executor-function-extensible.js',
+    'built-ins/Promise/reject-function-extensible.js',
+    'built-ins/Promise/resolve-function-extensible.js'
+  );
+}
 
-describe('test262 promise test suite', function(done) {
+describe('test262', function(done) {
   /* jshint evil:true */ // It doesn't like the `eval`s here.
   var $ERROR = function(msg) { throw new Error(msg); };
   var assert = new Function(
@@ -41,12 +73,16 @@ describe('test262 promise test suite', function(done) {
     fs.readFileSync(path.join(BASEDIR, '../harness/assert.js'), 'utf8') +
       '\n return assert;'
   )($ERROR);
+  var setPrototypeOf = Object.setPrototypeOf || function(o, p) {
+    /* jshint proto:true */
+    o.__proto__ = p;
+  };
   var Test262Error = function Test262Error(msg) {
     var self = new Error(msg);
-    Object.setPrototypeOf(self, Test262Error.prototype);
+    setPrototypeOf(self, Test262Error.prototype);
     return self;
   };
-  Object.setPrototypeOf(Test262Error, Error);
+  setPrototypeOf(Test262Error, Error);
   Test262Error.prototype = Object.create(Error.prototype);
   Test262Error.prototype.constructor = Test262Error;
 
@@ -69,6 +105,12 @@ describe('test262 promise test suite', function(done) {
       var itit = it;
       if (BLACKLIST.indexOf(shortName) >= 0) { itit = it.skip; }
       if (features.indexOf('class') >= 0) { itit = it.skip; }
+      var usesSymbol = features.filter(function(f) {
+        return /^Symbol/.test(f);
+      }).length > 0;
+      // Node 0.10 compatibility
+      if (typeof Symbol === 'undefined' && usesSymbol) { itit = it.skip; }
+      // Node doesn't yet have these symbols
       if (features.indexOf('Symbol.species') >= 0) { itit = it.skip; }
       if (features.indexOf('Symbol.toStringTag') >= 0) { itit = it.skip; }
       includes.forEach(function(f) {
