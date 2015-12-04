7a067d7c620df30713995763e290c3c11946f80e
C. Scott Ananian <cscott@cscott.net>
2015-12-04 15:56:02 -0500

    Trim down irrelevant packages in performance benchmark.

--- benchmark/lib/fakesP.js
+++ benchmark/lib/fakesP.js
@@ -1,61 +1,10 @@
 
 
-if (global.useQ)
-    var lifter = require('q').denodeify;
-else if (global.useBluebird)
+if (global.useBluebird)
     //Currently promisifies only Node style callbacks
     var lifter = require('../../js/release/bluebird.js').promisify;
-else if (global.useKew) {
-    var q = require('kew');
-    var slicer = [].slice;
-    var lifter = function lifter(nodefn) {
-        return function() {
-            var p = q.defer();
-            arguments[arguments.length++] = function(err, res) {
-                if (err) p.reject(err);
-                else p.resolve(res)
-            };
-            try {
-                nodefn.apply(this, arguments);
-            }
-            catch (e) {
-                p.reject(e);
-            }
-            return p;
-        }
-    }
-}
-else if(global.useLie) {
-    var Lie = require('lie');
-    var lifter = function(nodefn) {
-        return function() {
-            var self = this;
-            var l = arguments.length;
-            var args = new Array(l + 1);
-            for (var i = 0; i < l; ++i) {
-                args[i] = arguments[i];
-            }
-            return new Lie(function(resolve, reject) {
-                args[l] = function(err, val) {
-                    if (err) reject(err);
-                    else resolve(val);
-                };
-                nodefn.apply(self, args);
-            });
-        };
-    };
-}
 else if(global.useThenPromise) {
     var lifter = require("promise").denodeify;
-}
-else if( global.useRSVP ) {
-    var lifter = require("rsvp").denodeify;
-}
-else if( global.useDeferred) {
-    var lifter = require("deferred").promisify;
-}
-else if( global.useDavy) {
-    var lifter = require("davy").wrap;
 }
 else if (global.useNative) {
     try {
@@ -83,7 +32,7 @@ else if (global.useNative) {
     };
 }
 else {
-    var lifter = require('when/node').lift;
+    throw new Error("no lifter");
 }
 
 var f = require('./dummy');
