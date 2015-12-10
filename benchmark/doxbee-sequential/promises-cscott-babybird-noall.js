532efc91807d0a11d8ce12ad372615a475bf46d3
C. Scott Ananian <cscott@cscott.net>
2015-12-10 17:07:31 -0500

    Name all functions for easier profiling.

--- benchmark/doxbee-sequential/promises-cscott-babybird-noall.js
+++ benchmark/doxbee-sequential/promises-cscott-babybird-noall.js
@@ -9,10 +9,10 @@ module.exports = function upload(stream, idOrPath, tag, done) {
     var fileP = self.byUuidOrPath(idOrPath).get();
     var version, fileId, file, blobId;
 
-    blobIdP.then(function(_blobId) {
+    blobIdP.then(function bench1(_blobId) {
         blobId = _blobId;
         return fileP;
-    }).then(function(fileV) {
+    }).then(function bench2(fileV) {
         file = fileV;
         var previousId = file ? file.version : null;
         version = {
@@ -24,7 +24,7 @@ module.exports = function upload(stream, idOrPath, tag, done) {
         };
         version.id = Version.createHash(version);
         return Version.insert(version).execWithin(tx);
-    }).then(function() {
+    }).then(function bench3() {
         if (!file) {
             var splitPath = idOrPath.split('/');
             var fileName = splitPath[splitPath.length - 1];
@@ -34,27 +34,27 @@ module.exports = function upload(stream, idOrPath, tag, done) {
                 userAccountId: userAccount.id,
                 name: fileName,
                 version: version.id
-            }).then(function(q) {
+            }).then(function bench4(q) {
                 return q.execWithin(tx);
-            }).then(function() {
+            }).then(function bench5() {
                 return newId;
             });
         } else {
             return file.id;
         }
-    }).then(function(fileIdV) {
+    }).then(function bench6(fileIdV) {
         fileId = fileIdV;
         return FileVersion.insert({
             fileId: fileId,
             versionId: version.id
         }).execWithin(tx);
-    }).then(function() {
+    }).then(function bench7() {
         return File.whereUpdate({id: fileId}, {version: version.id})
             .execWithin(tx);
-    }).then(function() {
+    }).then(function bench8() {
         tx.commit();
         return done();
-    }, function(err) {
+    }, function bench9(err) {
         tx.rollback();
         return done(err);
     });
